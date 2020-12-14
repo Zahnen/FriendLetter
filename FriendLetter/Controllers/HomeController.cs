f6cbea539a437e4e72f132861554086be3aa62f4
Zahnen Garner <zahnen@gmail.com>
2020-12-13 18:00:08 -0800

    finalize additions to homework lesson

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
 
 namespace FriendLetter.Controllers
 {
@@ -12,8 +14,24 @@ namespace FriendLetter.Controllers
     public string Goodbye() { return "Goodbye friend."; }
 
     [Route("/")]
-    public ActionResult Letter() { return View(); }
+    public ActionResult Letter()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
+      return View(myLetterVariable);
+    }
 
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
 
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
   }
 }
